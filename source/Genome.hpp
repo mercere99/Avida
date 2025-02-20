7e201ceadc622f8bce3ec17482a54231fbf95810
Charles Ofria <charles.ofria@gmail.com>
2025-02-20 13:31:56 -0500

    Simplified StateGenome to just Genome

--- source/Genome.hpp
+++ source/Genome.hpp
@@ -13,84 +13,72 @@
 #include "emp/meta/TypePack.hpp"
 
 /// @brief A genome with a fixed number of possible states.
-/// @param NUM_STATES a count of how many states are possible at each position.
-template <size_t NUM_STATES=256>
-class StateGenome {
+class Genome {
 public:
-  using state_t = emp::min_uint_type<NUM_STATES>;
+  using value_t = uint8_t;
 
 private:
-  using this_t = StateGenome<NUM_STATES>;
-  using genome_t = emp::vector<state_t>;
-
-  genome_t genome;
+  emp::vector<value_t> genome;
 
 public:
-  StateGenome() = default;
-  StateGenome(const StateGenome &) = default;
-  StateGenome(StateGenome &&) = default;
-  StateGenome(const genome_t & g) : genome(g) { }
-  StateGenome(genome_t && g) : genome(std::move(g)) { }
-  StateGenome(size_t length, state_t default_value=0) : genome(length, default_value) { }
-  ~StateGenome() { }
+  Genome() = default;
+  Genome(const Genome &) = default;
+  Genome(Genome &&) = default;
+  Genome(const emp::vector<value_t> & g) : genome(g) { }
+  Genome(emp::vector<value_t> && g) : genome(std::move(g)) { }
+  Genome(size_t length, value_t default_value=0) : genome(length, default_value) { }
+  ~Genome() { }
 
   auto begin() { return genome.begin(); }
   auto begin() const { return genome.begin(); }
   auto end() { return genome.end(); }
   auto end() const { return genome.end(); }
 
-  StateGenome & operator=(const StateGenome &) = default;
-  StateGenome & operator=(StateGenome &&) = default;
+  Genome & operator=(const Genome &) = default;
+  Genome & operator=(Genome &&) = default;
 
-  [[nodiscard]] bool operator<=>(const StateGenome &) const = default;
+  [[nodiscard]] bool operator<=>(const Genome &) const = default;
 
-  [[nodiscard]] state_t operator[](size_t pos) const { return genome[pos]; }
-  [[nodiscard]] state_t & operator[](size_t pos) { return genome[pos]; }
+  [[nodiscard]] value_t operator[](size_t pos) const { return genome[pos]; }
+  [[nodiscard]] value_t & operator[](size_t pos) { return genome[pos]; }
 
-  [[nodiscard]] state_t Get(size_t pos) const { return genome[pos]; }
-  void Set(size_t pos, state_t val) {
-    emp_assert(val < NUM_STATES);
-    genome[pos] = val;
-  }
+  [[nodiscard]] value_t Get(size_t pos) const { return genome[pos]; }
+  void Set(size_t pos, value_t val) { genome[pos] = val; }
 
-  void Push(state_t val) { genome.push_back(val); }
+  void Push(value_t val) { genome.push_back(val); }
 
   [[nodiscard]] size_t size() const { return genome.size(); }
-  this_t & Resize(size_t new_size) {
+  Genome & Resize(size_t new_size) {
     genome.resize(new_size);
     return *this;
   }
+
+  void Reserve(size_t max_size) { genome.reserve(max_size); }
 
   template <typename T>
   void Insert(size_t pos, T && value, size_t count=1) {
     genome.insert(genome.begin()+pos, count, std::forward<T>(value));
   }
 
-  template <typename T>
   void Erase(size_t pos, size_t count=1) {
-    genome.erase(genome.begin()+pos, count);
+    const size_t end_pos = pos + count;
+    genome.erase(genome.begin()+pos, genome.begin()+end_pos);
   }
 
   // Remove [start_pos, end_pos) from this Vector and return it.
-  [[nodiscard]] this_t Copy(size_t start_pos, size_t count) {
+  [[nodiscard]] Genome Copy(size_t start_pos, size_t count) {
     const size_t end_pos = start_pos + count;
     emp_assert(start_pos <= size() && count <= size() && end_pos <= size());
-    this_t out;
-    out.reserve(count);
-    std::copy(begin() + start_pos, begin() + end_pos, std::back_inserter(out));
+    Genome out;
+    out.Reserve(count);
+    std::copy(begin() + start_pos, begin() + end_pos, std::back_inserter(out.genome));
     return out;
   }
 
   // Remove [start_pos, end_pos) from this Vector and return it.
-  [[nodiscard]] this_t Extract(size_t start_pos, size_t count) {
-    this_t out(Copy(start_pos, count));
+  [[nodiscard]] Genome Extract(size_t start_pos, size_t count) {
+    Genome out(Copy(start_pos, count));
     Erase(start_pos, count);
     return out;
-  }
-
-  bool OK() const {
-    // Make sure all states are legal.
-    for (auto state : genome) { if (state >= NUM_STATES) return false; }
-    return true;
   }
 };
