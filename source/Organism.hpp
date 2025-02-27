4f7c94c2f9c613fb69ddb2b13c461a62953ced98
Charles Ofria <charles.ofria@gmail.com>
2025-02-27 17:03:34 -0500

    Extended Organism to handle generations, ids, and input genomes.

--- source/Organism.hpp
+++ source/Organism.hpp
@@ -14,9 +14,22 @@
 /// All organisms in Avida are set up as an Organism class.
 class Organism {
 private:
+  using id_t = uint64_t;
+  using gen_t = uint32_t;
+  static constexpr id_t UNKNOWN_ID = static_cast<id_t>(-1);
+
+  HardwareManager & hw_man;
   emp::Ptr<HardwareBase> hw_ptr;
 
-  uint32_t generation = 0; 
+  id_t id = UNKNOWN_ID;     // Unique organism ID.
+  gen_t generation = 0; 
 public:
-  Organism(HardwareManager & hm) : hw_ptr(hm.Allocate()) { }
+  Organism(HardwareManager & hw_man, Genome genome)
+    : hw_man(hw_man), hw_ptr(hw_man.Allocate(*this)) { hw_ptr->Reset(genome); }
+
+  id_t ID() const { return id; }
+  Organism & ID(id_t in_id) { id = in_id; return *this; }
+
+  gen_t Generation() const { return generation; }
+  Organism & Generation(gen_t in_gen) { generation = in_gen; return *this; }
 };
