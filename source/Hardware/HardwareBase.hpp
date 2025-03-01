9816cb868a986a552901fea6840daddb6bae20e5
Charles Ofria <charles.ofria@gmail.com>
2025-03-01 18:22:27 -0500

    Set up HardwareBase to track both its organism and its HardwareManager.

--- source/Hardware/HardwareBase.hpp
+++ source/Hardware/HardwareBase.hpp
@@ -8,9 +8,20 @@
 
 #include "../Genome.hpp"
 
+// Forward declarations
+class HardwareManager;
+class Organism;
+
 class HardwareBase {
+protected:
+  HardwareManager & hw_manager;
+  emp::Ptr<Organism> org_ptr = nullptr;
 public:
+  HardwareBase(HardwareManager & hw_manager) : hw_manager(hw_manager) { }
   virtual ~HardwareBase() { }
+
+  Organism & GetOrganism() { emp_assert(org_ptr); return *org_ptr; }
+  HardwareBase & SetOrganism(Organism & org) { org_ptr = &org; return *this; }
 
   virtual void Run(size_t cycles=10) = 0;
   virtual void Reset() = 0;
