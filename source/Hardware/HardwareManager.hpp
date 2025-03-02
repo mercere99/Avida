68f557ce62efd1ed5b6cabc3708e19575d676d7e
Charles Ofria <charles.ofria@gmail.com>
2025-03-02 14:07:37 -0500

    Made HardwareManager more aware of Organisms.

--- source/Hardware/HardwareManager.hpp
+++ source/Hardware/HardwareManager.hpp
@@ -6,29 +6,42 @@
  *  Released under the MIT Public Licence.  See LICENSE.md for details.
  */
 
+// Hardware managers build new hardware for Organisms and recycle old hardware
+// to speed up organism construction.
+
 #include "emp/base/Ptr.hpp"
 #include "emp/base/vector.hpp"
+#include "emp/tools/String.hpp"
 
 #include "HardwareBase.hpp"
 #include "InstSet.hpp"
+
+// Pre-declarations
+class Organism;
 
 /// A class to handle a particular configuration of hardware.
 class HardwareManager {
 protected:
   using hw_ptr_t = emp::Ptr<HardwareBase>;
+  using feedback_t = std::function<void(Organism & /*org*/)>;
   emp::vector< hw_ptr_t > hw_ptrs;  // Pointers to available hardware.
 
-  virtual hw_ptr_t AllocateNew() = 0;
+  virtual hw_ptr_t AllocateNew(Organism & org) = 0;
 public:
+  virtual ~HardwareManager() { }
+
+  virtual bool AddCallback(emp::String name, feedback_t fun) = 0;
+
   virtual Genome LoadGenome(emp::String filename) = 0;
 
-  hw_ptr_t Allocate() {
+  hw_ptr_t Allocate(Organism & org) {
     if (hw_ptrs.size()) {
       hw_ptr_t out = hw_ptrs.back();
       hw_ptrs.pop_back();
+      out->SetOrganism(org);
       return out;
     }
-    return AllocateNew();
+    return AllocateNew(org);
   }
 
   void Release(hw_ptr_t ptr) {
@@ -40,11 +53,23 @@ template <typename VM_T>
 class HardwareType : public HardwareManager {
 private:
   using HardwareManager::hw_ptr_t;
-  hw_ptr_t AllocateNew() override { return emp::NewPtr<VM_T>(); }
+  hw_ptr_t AllocateNew(Organism & org) override {
+    auto out_ptr = emp::NewPtr<VM_T>(*this);
+    out_ptr->SetOrganism(org);
+    return out_ptr;
+}
 
-  InstSet inst_set;
+  InstSet<VM_T> inst_set;
 public:
   HardwareType() : inst_set(VM_T::BuildInstSet()) { }
+
+  InstSet<VM_T> & GetInstSet() { return inst_set; }
+  const InstSet<VM_T> & GetInstSet() const { return inst_set; }
+
+  bool AddCallback(emp::String name, feedback_t fun) override {
+    inst_set.AddCallbackInst(name, fun);
+    return true;
+  }
 
   // Load an organism from a file.
   Genome LoadGenome(emp::String filename) override {
