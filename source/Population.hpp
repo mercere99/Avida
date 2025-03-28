accad853e8102cea7f377a9d9943149ef7b18781
Charles Ofria <charles.ofria@gmail.com>
2025-03-27 22:24:42 -0400

    Setup scheduler to distribute CPU cycles.

--- source/Population.hpp
+++ source/Population.hpp
@@ -1,10 +1,15 @@
+#pragma once
+
 /*
  *  This file is part of the Avida Digital Evolution Research Platform, v5.0
  *  Copyright (C) Michigan State University & Dr. Charles Ofria
  *  Released under the MIT Public Licence.  See LICENSE.md for details.
  */
 
+#include <numeric>
+
 #include "emp/base/Ptr.hpp"
+#include "emp/datastructs/UnorderedIndexMap.hpp"
 #include "emp/math/Random.hpp"
 
 #include "Organism.hpp"
@@ -14,11 +19,15 @@ class Population {
 private:
   emp::vector<Organism> orgs{};
   emp::Random & random;
+  emp::UnorderedIndexMap speed_map; // Track how fast each CPU should be going.
 
   size_t max_size = static_cast<size_t>(-1); // Population cap.
 
   Population & SwapOrgs(size_t id1, size_t id2) {
-    if (id1 != id2) std::swap(orgs[id1], orgs[id2]);
+    if (id1 != id2) {
+      std::swap(orgs[id1], orgs[id2]);
+      speed_map.Swap(id1, id2);
+    }
     return *this;
   }
 
@@ -38,16 +47,15 @@ private:
     emp_assert(org.OK());
     emp_assert(orgs.size() <= max_size);
 
-    // See if we must delete an organism before adding a new one.
+    // See if we must delete an organism before we can add a new one.
     if (orgs.size() == max_size) DeleteOrg();
 
-    emp_assert(org.OK());
-    // std::cout << "BEFORE:    "; org.Print();
+    uint32_t index = orgs.size();
     orgs.push_back(std::move(org));
-    // std::cout << "\nAFTER OLD: "; org.Print();
-    // std::cout << "\nAFTER NEW: "; orgs.back().Print();
-    emp_assert(orgs.back().OK());
-    orgs.back().SetPopulation(*this);
+    Organism & new_org = orgs.back();
+    emp_assert(new_org.OK());
+    new_org.SetPopulation(*this, index);
+    speed_map.Set(index, new_org.GetMetabolicRate());
     return *this;
   }
 
@@ -59,13 +67,20 @@ public:
 
   Population & SetMaxSize(size_t in) { max_size = in; return *this; }
 
+  double GetAveGeneration() {
+    if (orgs.empty()) return 0.0;  // An empty population has no generations.
+    size_t total = std::accumulate(orgs.begin(), orgs.end(), 0.0,
+                   [](size_t total, Organism & org){ return total + org.GetGeneration(); });
+    return total / static_cast<double>(orgs.size());
+  }
+
   Organism & operator[](size_t id) { emp_assert(id < orgs.size()); return orgs[id]; }
   const Organism & operator[](size_t id) const { emp_assert(id < orgs.size()); return orgs[id]; }
 
   template <typename T>
   requires std::same_as<std::remove_cvref_t<T>, Genome>
   Population & Inject(HardwareManager & hw_man, T && genome) {
-      return Insert(Organism{hw_man, std::forward<T>(genome)});
+    return Insert(Organism{hw_man, std::forward<T>(genome)});
   }
   
   /// @brief Inject an organism using a genome loaded from a file.
@@ -86,7 +101,9 @@ public:
     emp_assert(orgs.size());
     static constexpr int CPU_CHUNK = 10;
     while (cycles > 0) {
-      size_t id = random.GetUInt(orgs.size());
+      size_t id = speed_map.Index(random.GetDouble(speed_map.GetWeight()));
+      emp_assert(id < size());
+      // random.GetUInt(orgs.size());
       orgs[id].Process(CPU_CHUNK);
       cycles -= CPU_CHUNK;
     }
