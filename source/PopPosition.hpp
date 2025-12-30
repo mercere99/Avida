ce6e0d340c46185a1be183b97be7b4732af3f113
Charles Ofria <charles.ofria@gmail.com>
2025-12-30 13:25:36 -0500

    Adjusted PopPosition to work on specific population types.

--- source/PopPosition.hpp
+++ source/PopPosition.hpp
@@ -9,26 +9,25 @@
 #include "emp/base/Ptr.hpp"
 #include "emp/math/constants.hpp"
 
-class Population;
-
+template <typename POP_T>
 class PopPosition {
 public:
   using pos_t = uint32_t;
   static constexpr pos_t npos = static_cast<pos_t>(-1);
 
 private:
-  emp::Ptr<Population> pop_ptr = nullptr;  // Which population is this position in?
+  emp::Ptr<POP_T> pop_ptr = nullptr;  // Which population is this position in?
   pos_t index = npos;
 
 public:
-  Population & GetPopulation() { return *pop_ptr; }
-  const Population & GetPopulation() const { return *pop_ptr; }
-  PopPosition & SetPopulation(Population & pop) { pop_ptr = &pop; return *this;}
+  POP_T & GetPopulation() { return *pop_ptr; }
+  const POP_T & GetPopulation() const { return *pop_ptr; }
+  PopPosition & SetPopulation(POP_T & pop) { pop_ptr = &pop; return *this;}
 
   pos_t GetIndex() const { return index; }
   PopPosition & SetIndex(pos_t in) { index = in; return *this; }
 
-  PopPosition & Set(Population & pop, pos_t in) {
+  PopPosition & Set(POP_T & pop, pos_t in) {
     pop_ptr = &pop;
     index = in;
     return *this;
@@ -40,5 +39,5 @@ public:
   }
 
   bool InPopulation() const { return !pop_ptr.IsNull(); }
-  bool InPopulation(const Population & test_pop) const { return pop_ptr == &test_pop; }
+  bool InPopulation(const POP_T & test_pop) const { return pop_ptr == &test_pop; }
 };
