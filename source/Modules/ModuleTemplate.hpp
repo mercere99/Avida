0376600bc19fbd96e88aada8ee01c67580a459b2
Charles Ofria <charles.ofria@gmail.com>
2026-03-19 14:14:47 -0400

    Added Register functions and new triggers to ModuleTemplate

--- source/Modules/ModuleTemplate.hpp
+++ source/Modules/ModuleTemplate.hpp
@@ -17,45 +17,73 @@ class ModuleTemplate : public ModuleBase<AVIDA_T> {
 private:
   AVIDA_T & avida;
 
+  int example_setting;
+
 public:
   ModuleTemplate(AVIDA_T & avida)
     : ModuleBase<AVIDA_T>("ModuleName", "ModuleType", "Module description text"), avida(avida) { }
   ~ModuleTemplate() { }
 
-  // === Phenotypic Traits ===
+  // === General Configuration ===
 
-  // struct Phenotype {
-  //   AVIDA_TRAIT(size_t, generation, "Number of offspring in chain since inject");
-  // };
+  struct Phenotype {
+    size_t example_trait = 0;
+  };
+
+  void RegisterTraits() {
+    AVIDA_REGISTER_TRAIT(example_trait, "Describe your trait here");
+  }
+
+  void RegisterSettings() {
+    avida.AddSetting("example_setting", example_setting, "Description of example setting", 'x');
+  }
+
+  void RegisterCallbacks() {
+    avida.AddCallback("DivideCell", [this](size_t biota_id){ avida.DivideOrg(biota_id); });
+  }
 
   // === Signal Listeners ===
 
+  // Triggered: Once a the very beginning of a run, after all configurations have been loaded.
+  void OnStart() { }
+
+  // Triggered: Every update at the very beginning of the update.
   void OnUpdateStart([[maybe_unused]] size_t new_update) { }
 
+  // Triggered: Every update at the end of the update.
   void OnUpdateEnd([[maybe_unused]] size_t old_update) { }
 
+  // Triggered: When a parent is about to replicate, before starting to make the new organism.
   template <concepts::Organism ORG_T>
   void BeforeRepro([[maybe_unused]] ORG_T & parent) { }
 
+  // Triggered: Offspring Organism has been identified and supplied genome (do mutations here!)
   template <concepts::Organism ORG_T>
   void OnOffspringInit([[maybe_unused]] ORG_T & org, [[maybe_unused]] ORG_T & parent) { }
 
+  // Triggered: All other aspects of building an offspring are complete; last connection to parent.
   template <concepts::Organism ORG_T>
   void OnOffspringReady([[maybe_unused]] ORG_T & offspring, [[maybe_unused]] ORG_T & parent) { }
 
+  // Triggered: Organism has been build from scratch to be injected into the population.
   template <concepts::Organism ORG_T>
   void OnInjectReady([[maybe_unused]] ORG_T & inject_org) { }
 
+  // Triggered: Organism (either offspring or injected) is about the be placed into a population.
   template <concepts::Organism ORG_T>
   void BeforePlacement([[maybe_unused]] ORG_T & org) { }
 
+  // Triggered: Organism has been placed in the population.
   template <concepts::Organism ORG_T>
   void OnPlacement([[maybe_unused]] ORG_T & org) { }
 
+  // Triggered: Organism is about to die.
   template <concepts::Organism ORG_T>
   void BeforeDeath([[maybe_unused]] ORG_T & org) { }
 
+  // Triggered: Run is about to end.
   void BeforeExit() { }
-  
+
+  // Triggered: User has requested help.
   void OnHelp() { }
 };
