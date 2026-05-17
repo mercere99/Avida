e66aeb29868a72b1d6a888049f47fb2731d1310f
Charles Ofria <charles.ofria@gmail.com>
2026-05-16 23:57:07 -0400

    Added some helper function to Biota for organism analysis

--- source/core/Biota.hpp
+++ source/core/Biota.hpp
@@ -36,7 +36,7 @@ public:
   [[nodiscard]] size_t GetSize() const { return orgs.size(); }
   [[nodiscard]] uint32_t GetNumOrgs() const { return num_orgs; }
   [[nodiscard]] bool IsActive(size_t id) const { return active_bits.Get(id); }
-  [[nodiscard]] const emp::BitVector & GetActiveSet() const { return active_bits; }
+  [[nodiscard]] const emp::BitVector & GetActiveBits() const { return active_bits; }
   [[nodiscard]] size_t GetTotalOrgs() const { return total_orgs; }
 
   [[nodiscard]] auto & GetOrg(this auto & self, size_t id) {
@@ -97,6 +97,56 @@ public:
     orgs.clear();
     active_bits.Clear();
     num_orgs = 0;
+  }
+
+  /// Return the biota ID of the organisms with the minimum value in this function.
+  template <std::invocable<const organism_t &> FUN_T>
+  [[nodiscard]] size_t FindMinimumID(const FUN_T & fun) const {
+    double min_found = std::numeric_limits<double>::max();
+    size_t min_index = 0;
+    for (size_t index : active_bits) {
+      double value = fun(orgs[index]);
+      if (value < min_found) {
+        min_found = value;
+        min_index = index;
+      }
+    }
+    return min_index;
+  }
+
+  /// Return the biota ID of the organisms with the maximum value in this function.
+  template <std::invocable<const organism_t &> FUN_T>
+  [[nodiscard]] size_t FindMaximumID(const FUN_T & fun) const {
+    double max_found = std::numeric_limits<double>::lowest();
+    size_t max_index = 0;
+    for (size_t index : active_bits) {
+      double value = fun(orgs[index]);
+      if (value > max_found) {
+        max_found = value;
+        max_index = index;
+      }
+    }
+    return max_index;
+  }
+
+  /// Return the minimum value in this function across all organisms.
+  template <std::invocable<const organism_t &> FUN_T>
+  [[nodiscard]] double CalcMinimum(const FUN_T & fun) const {
+    double min_found = std::numeric_limits<double>::max();
+    for (size_t index : active_bits) {
+      min_found = std::min<double>(min_found, fun(orgs[index]));
+    }
+    return min_found;
+  }
+
+  /// Return the maximum value in this function across all organisms.
+  template <std::invocable<const organism_t &> FUN_T>
+  [[nodiscard]] double CalcMaximum(const FUN_T & fun) const {
+    double max_found = std::numeric_limits<double>::lowest();
+    for (size_t index : active_bits) {
+      max_found = std::max<double>(max_found, fun(orgs[index]));
+    }
+    return max_found;
   }
 
   template <std::invocable<const organism_t &> FUN_T>
