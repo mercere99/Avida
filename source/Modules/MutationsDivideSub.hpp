66f0cced97b4104f6b7eb2223fbbfa838882829c
Charles Ofria <charles.ofria@gmail.com>
2026-08-06 17:12:48 -0400

    Setup adjustment of mutation rates.

--- source/Modules/MutationsDivideSub.hpp
+++ source/Modules/MutationsDivideSub.hpp
@@ -8,6 +8,7 @@
  *  This is a starter plug-in module, with all of the signals listed, but none set to do anything.
  */
 
+#include <cmath>
 #include <cstddef>   // for size_t
 #include <fstream>
 #include <iostream>
@@ -22,6 +23,21 @@ private:
   double substitution_prob{0.0075};
   double mut_scale{1.0 / emp::Log2(1.0 - substitution_prob)};
 
+  void SetSubstitutionProb(double probability) {
+    if (!std::isfinite(probability) || probability < 0.0 || probability > 1.0) {
+      emp::notify::Error(
+        "mutations.substitution_prob must be between 0.0 and 1.0; received ",
+        probability,
+        "."
+      );
+    }
+
+    substitution_prob = probability;
+    if (substitution_prob > 0.0 && substitution_prob < 1.0) {
+      mut_scale = 1.0 / emp::Log2(1.0 - substitution_prob);
+    }
+  }
+
 public:
   MutationsDivideSub(AVIDA_T & avida)
     : ModuleBase<AVIDA_T>(avida, "MutationsDivideSub", "Mutation",
@@ -35,7 +51,7 @@ public:
   void RegisterSettings() {
     avida.AddSetting("mutations.substitution_prob",
       [this](){ return substitution_prob; },
-      [this](double p){ substitution_prob = p; mut_scale = 1.0 / emp::Log2(1.0 - substitution_prob); },
+      [this](double p){ SetSubstitutionProb(p); },
       "Per-site substitution probability", 'p');
   }
 
@@ -43,8 +59,19 @@ public:
 
   template <concepts::Organism ORG_T>
   void OnOffspringInit(ORG_T & org, [[maybe_unused]] ORG_T & parent) {
+    if (substitution_prob == 0.0) return;
+
     auto & genome = org.GetGenome();
     emp::Random & random = avida.GetRandom();
+
+    if (substitution_prob == 1.0) {
+      for (size_t mut_pos = 0; mut_pos < genome.size(); ++mut_pos) {
+        genome.RandomizeSite(random, mut_pos);
+      }
+      org.SetMutated();
+      return;
+    }
+
     size_t mut_pos = static_cast<size_t>(std::log2(random.GetDoubleNonZero()) * mut_scale);
     while (mut_pos < genome.size()) {
       genome.RandomizeSite(random, mut_pos);
